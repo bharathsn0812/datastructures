d86173ac638cd9e05b3f9be1895ad463b6aca6c5
bharath <bharathsn0812@gmail.com>
2021-04-29 10:43:38 +0100

    adding binary heap

---
+++ binaryHeap/MaxBinaryHeap.js
@@ -0,0 +1,86 @@
+class MaxBinaryHeap {
+    constructor () {
+        this.values = []
+    }
+
+    insert (value) {
+        this.values.push(value)
+        this.bubbleUp()
+    }
+
+    bubbleUp () {
+        let idx = this.values.length - 1
+        let element = this.values[idx]
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1)/2)
+            let parent = this.values[parentIdx]
+
+            if (element <= parent) {
+                break
+            }
+            
+            this.values[parentIdx] = element
+            this.values[idx] = parent
+            idx = parentIdx
+        }
+    }
+
+    extractMax () {
+        const max = this.values[0]
+        const end = this.values.pop()
+        this.values[0] = end
+        if (this.values.length > 1) {
+            this.bubbleDown()
+        }
+        return max
+    }
+
+    bubbleDown () {
+        let idx = 0
+        const length = this.values.length
+        const element = this.values[0]
+
+        while (true) {
+            let leftChildIdx = 2 * idx + 1
+            let rightChildIdx = 2 * idx + 2
+            let leftChild, rightChild
+            let swap = null
+
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx]
+                if (leftChild > element) {
+                    swap = leftChildIdx
+                }
+            }
+
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx]
+                if (
+                    (swap === null && rightChild > element) ||
+                    (swap !== null && rightChild> leftChild)
+                ) {
+                    swap = rightChildIdx
+                }
+            }
+
+            if (!swap) {
+                break
+            }
+
+            this.values[idx] = this.values[swap]
+            this.values[swap] = element
+            idx = swap
+        }
+    }
+}
+
+let heap = new MaxBinaryHeap()
+heap.insert(41)
+heap.insert(39)
+heap.insert(33)
+heap.insert(18)
+heap.insert(27)
+heap.insert(12)
+heap.insert(55)
+console.log(heap.values)
+console.log(heap.extractMax())
